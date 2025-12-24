962cdd5b2ad115d6137c4a75e33a2d7560bb3be4
Zach Malter <zach@maltertech.com>
2025-12-24 14:43:19 -0500

    refactor CloudWatchForwarder: improve comments and enhance log event handling

--- src/CloudWatchForwarder.php
+++ src/CloudWatchForwarder.php
@@ -31,37 +31,54 @@ class CloudWatchForwarder
      */
     public function push(array $payload): void
     {
-        // 1. Base64 decode
+        // Base64 decode
         if (!isset($payload["awslogs"]["data"])) {
             return;
         }
-
         $decodedData = base64_decode($payload["awslogs"]["data"]);
 
-        // 2. Gzip decompress
+        // Gzip decompress
         $decompressedData = gzdecode($decodedData);
-
         if ($decompressedData === false) {
             return;
         }
 
-        // 3. JSON decode
+        // json decode
         $parsedData = json_decode($decompressedData, true);
-
         if (!isset($parsedData['logEvents'])) {
             return;
         }
 
+        // extract logs
         $events = [];
 
+        // Each event in the logEvents array has an id, timestamp and message key.
+        /* Example below
+        {
+            "logEvents": [
+                {
+                    "id": "35286272357882262515405211100061592492480036482215494656",
+                    "timestamp": 1631531111000,
+                    "message": "NOTICE: PHP message: PHP Caught Error: Webhook skipped"
+                },
+                {
+                    "id": "35286272357882262515405211100061592492480036482215494657",
+                    "timestamp": 1631531111000,
+                    "message": "{ \"duration\": 14, \"path\": \"/\", \"requestId\": \"TajXi_KyKL4EMoA=\", \"status\": 200 }"
+                }
+            ],
+         */
         foreach ($parsedData["logEvents"] as $event) {
             // Skip Lambda start/stop request messages
             if (isset($event["message"]) && str_contains($event["message"], "RequestId:")) {
                 continue;
             }
 
-            // Check if message is valid JSON (AWS API Gateway logs) or String (Lambda logs)
-            // Note: json_validate requires PHP 8.3+
+            // There are two types of messages:
+            // 1) Lambda Format (String): NOTICE: PHP message: PHP Caught Error: Webhook skipped
+            // 2) API Gateway Format (JSON String): { "duration": 14, "path": "/", "requestId": "TajXi_KyKL4EMoA=", "status": 200 }
+
+            // check if the message is a valid JSON, and if so, push it to the events array, otherwise push it as a string
             if (isset($event["message"]) && json_validate($event["message"])) {
                 $parsedEvent = json_decode($event["message"], true);
             } else {
@@ -74,13 +91,14 @@ class CloudWatchForwarder
             $events[] = $parsedEvent;
         }
 
+        // If no events to send, return
         if (empty($events)) {
             return;
         }
 
-        // 4. Send to BetterStack (Curl created on demand)
+        // send to BetterStack, #1/#2 example: [{"message":"NOTICE: PHP message: PHP Caught Error: Webhook skipped"},{ "duration": 14, "path": "/", "requestId": "TajXi_KyKL4EMoA=", "status": 200 }]
         $curl = new Curl();
-        $curl->setHeader("Authorization", "Bearer {$this->sourceToken}");
+        $curl->setHeader("Authorization", "Bearer $this->sourceToken");
         $curl->setHeader("Content-Type", "application/json");
         $curl->post($this->ingestionURL, $events);
         $curl->close();
